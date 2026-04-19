d0f8bcfa835a6a6ff96b307c16dc2327b7248ce6
jodokfehlmann <jodok.fehlmann@gmail.com>
2026-04-19 18:40:53 +0200

    added meteo

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -32,9 +32,8 @@ const projects = [
     level: 'Débutant',
     levelClass: 'badge-debutant',
     duration: '3–4 heures',
-    link: '#',
+    link: '/docs/projet-meteo/introduction',
     color: '#0D6E6E',
-    comingSoon: true,
   },
   {
     emoji: '🤖',
